13eebb261202b87a0719cdf14e2743bad096b0fb
Vedaant Patel <vedaant@dhcp-172-31-098-036.mobile.uci.edu>
2024-01-09 18:25:53 -0800

    Fixed calculating votes

--- index.js
+++ index.js
@@ -500,7 +500,7 @@ function calcVotes(serverCode, endTimer) {
             if (endTimer) {
                 io.to(serverCode).emit("songVoted", {songURI: orderedList[0]});
                 orderedList.shift();
-                delete activeServers[serverCode].votes[orderList[0].uri];
+                delete activeServers[serverCode].votes[orderedList[0].uri];
                 // delete the voted song from list
             }
 
